392036f5f0fa25445bfb5da33620e630c454b8bb
G-Flodd <georgedodd@hotmail.co.uk>
2023-02-19 22:03:15 +0000

    Set up and questions added

---
+++ Assets/index.js
@@ -0,0 +1,61 @@
+const fs = require("fs");
+const path = require('path');
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "what is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide a project description"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please provide the installation instructions"
+    },
+    {
+        type: "input",
+        name: "licence",
+        message: "what licence is being used?"
+    },
+    {
+        type: "input",
+        name: "contributers",
+        message: "who contributed to this project?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "What software is being used?"
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Enter your GitHub link here"
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Enter your website link here"
+    }
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
